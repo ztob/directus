83381325cd9fc97dfe7d764b6dd29a66cb350909
Andrei Botezatu <btzsoft@gmail.com>
2021-08-26 21:08:18 +0300

    add basic iframe component

---
+++ app/src/interfaces/iframe/iframe.vue
@@ -0,0 +1,48 @@
+<template>
+	<div>
+		<iframe :width="widthAttribute" :height="heightAttribute" :src="urlAttribute"></iframe>
+	</div>
+</template>
+
+<script lang="ts">
+import { useI18n } from 'vue-i18n';
+import { defineComponent, PropType } from 'vue';
+import { Field } from '@directus/shared/types';
+export default defineComponent({
+	props: {
+		value: {
+			type: Array as PropType<Record<string, any>[]>,
+			default: null,
+		},
+		fields: {
+			type: Array as PropType<Partial<Field>[]>,
+			default: () => [],
+		},
+		template: {
+			type: String,
+			default: null,
+		},
+		disabled: {
+			type: Boolean,
+			default: false,
+		},
+		urlAttribute: {
+			type: String,
+			default: '',
+		},
+		widthAttribute: {
+			type: String,
+			default: '',
+		},
+		heightAttribute: {
+			type: String,
+			default: '',
+		},
+		collection: {
+			type: String,
+			default: null,
+		},
+	},
+	emits: ['input'],
+});
+</script>
